f12c9be8d6a823abad6910457bc448eb2a8c1ef4
Abhilash Thaduka <athaduka@gainsight.com>
2017-04-08 21:51:09 +0530

    reinitialized files

---
+++ src/main/java/com/gainsight/releaseteam/BetaRelease.java
@@ -0,0 +1,125 @@
+package com.gainsight.releaseteam;
+
+import com.gainsight.exceptions.AutomationException;
+import com.gainsight.pageobject.core.fluent.FluentDriver;
+import com.gainsight.sfdc.SalesforceConnector;
+import com.gainsight.sfdc.beans.SFDCInfo;
+import com.gainsight.testdriver.Log;
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.PropertyConfigurator;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.invoke.MethodHandles;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Properties;
+
+import static org.openqa.selenium.By.id;
+import static org.openqa.selenium.By.xpath;
+
+/**
+ * Created by Abhilash Thaduka on 3/13/2017.
+ */
+public class BetaRelease {
+
+    private static FluentDriver fluentDriver;
+    private static WebDriver driver;
+    private static Properties properties;
+    private static final String UPLOAD_BUTTON = "//input[@value='Upload']";
+    private static final String FINAL_BETA_UPLOAD_BUTTON = "//input[contains(@id,'PackageDetailsPageBlockButtons:upload')]";
+    private static final String UPLOAD_VERSION_NUMBER = "//span[contains(@id,'UploadVersionNumber')]";
+    private static final String VERSION_NAME_INPUT = "//input[contains(@id,'VersionInfoSectionItem:VersionText')]";
+    private static String confDir = ".";
+    private static String basedir = ".";
+    private static String sfdcendpoint;
+
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        loadPropertiesfile();
+        getSalesforceConnection();
+        driver = getChromeDriver();
+        getfd().get(properties.getProperty("sfdcurl"));
+        getfd().element(id("username")).waitUntil(30).ifElementIsNotDisplayed().clear().sendKeys(properties.getProperty("sfdcusername"));
+        getfd().element(id("password")).clear().sendKeys(properties.getProperty("sfdcpassword"));
+        getfd().element(id("Login")).click();
+        Thread.sleep(5000); // waiting for things to get settled
+        Log.info("Opening url for beta upload " + sfdcendpoint);
+        getfd().get(sfdcendpoint);
+        getfd().input(xpath(UPLOAD_BUTTON)).waitUntil(45).ifElementIsNotDisplayed().click();
+        String versionNumber = getfd().element(xpath(UPLOAD_VERSION_NUMBER)).waitUntil(600).ifElementIsNotDisplayed().getText().trim();
+        Calendar calendar = Calendar.getInstance();
+        String monthName = calendar.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.ENGLISH);
+        int year = calendar.get(Calendar.YEAR);
+        String finalVersion = "V " + versionNumber + " (" + monthName + " " + year + ")";
+        Log.info("=============Final version to upload is =========== " + finalVersion);
+        getfd().element(xpath(VERSION_NAME_INPUT)).waitUntil(30).ifElementIsNotDisplayed().clear().sendKeys(finalVersion);
+        //final upload page
+        //   getfd().input(xpath(FINAL_BETA_UPLOAD_BUTTON)).waitUntil(45).ifElementIsNotDisplayed().click();
+
+    }
+
+
+    private static void loadPropertiesfile() throws IOException {
+        properties = new Properties();
+        String propFileName = "betarelease.properties";
+        InputStream inputStream = MethodHandles.lookup().lookupClass().getClassLoader().getResourceAsStream(propFileName);
+        if (inputStream != null) {
+            properties.load(inputStream);
+        } else {
+            throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+        }
+
+        File configFolder = new File(basedir + File.separator + "conf");
+        if (!configFolder.exists()) {
+            confDir = new File(new File("").getAbsolutePath()).toPath().getParent().toString();
+        } else
+            confDir = basedir;
+        PropertyConfigurator.configure(confDir + "/conf/log4j.properties");
+    }
+
+    private static WebDriver getChromeDriver() {
+        String chromeDriverLocation = System.getProperty("webdriver.chrome.driver");
+        if (StringUtils.isEmpty(chromeDriverLocation)) {
+            throw new IllegalArgumentException("webdriver.chrome.driver property should not be null");
+        }
+        File chromeDriverExecutable = new File(chromeDriverLocation);
+        if (!chromeDriverExecutable.exists()) {
+            throw new AutomationException("webdriver.chrome.driver not found: " + chromeDriverLocation);
+        }
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        capabilities.setCapability("chrome.switches", Collections.singletonList("--start-maximized"));
+        driver = new ChromeDriver(capabilities);
+        driver.manage().window().maximize();
+        return driver;
+    }
+
+    private static FluentDriver getfd() {
+        if (fluentDriver == null)
+            initFluentDriver();
+        return fluentDriver;
+    }
+
+    private static void initFluentDriver() {
+        fluentDriver = new FluentDriver(driver);
+    }
+
+    private static void getSalesforceConnection() {
+        SalesforceConnector salesforceConnector = new SalesforceConnector(properties.getProperty("sfdcusername"),
+                properties.getProperty("sfdcpassword") + properties.getProperty("sfdcstoken"),
+                properties.getProperty("sfdcpartnerUrl"),
+                properties.getProperty("sfdcapiVersion"));
+        Assert.assertTrue(salesforceConnector.connect(), "Error while connecting to salesforce");
+        SFDCInfo sfdcInfo = salesforceConnector.fetchSFDCinfo();
+        sfdcendpoint = "https://" + sfdcInfo.getEndpoint().substring(8, sfdcInfo.getEndpoint().indexOf(".")) + ".salesforce.com/033U0000000CdVi?tab=PackageComponents";
+    }
+}
+
---
+++ src/main/java/com/gainsight/releaseteam/PushUpgrades.java
@@ -0,0 +1,140 @@
+package com.gainsight.releaseteam;
+
+import com.gainsight.exceptions.AutomationException;
+import com.gainsight.pageobject.core.fluent.FluentDriver;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import org.apache.commons.lang.StringUtils;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.*;
+import java.lang.invoke.MethodHandles;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.stream.Collectors;
+
+import static org.openqa.selenium.By.id;
+import static org.openqa.selenium.By.xpath;
+
+/**
+ * Created by Abhilash Thaduka on 2/22/2017.
+ */
+public class PushUpgrades {
+
+    private static FluentDriver fluentDriver;
+    private static WebDriver driver;
+    private static Properties properties;
+    private static final String SELECT_SFDC_VERSION = "//select[contains(@id,'SchedulePushUpgradePage')]";
+    private static final String ORGS_TYPE = "//select[contains(@id,'orgsType')]";
+    private static final String CURRENT_SFDC_VERSION = "//select[contains(@id,'eligibleVersions')]";
+    private static final String ORG_SEARCH_BOX = "//input[contains(@name,'SchedulePushUpgradePage:schedulePushUpgrade') and contains(@name,'searchBox')]";
+    private static final String SEARCH_RESULTS = "//table[contains(@id,'SchedulePushUpgradePage')]/tbody/tr";
+    private static final String LOADING_ICON = "//div[@class='loading']";
+    private static final String SELECT_ORG_ID = "//input[contains(@id,'selectedSingleOrg')]";
+    private static final String ORG_ID = "//span[contains(@id,'orgId')]";
+
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        List<String> orgs = getOrgDetails().stream().distinct().collect(Collectors.toList());
+        System.out.println("Total Org Id's list is " + orgs);
+        loadPropertiesfile();
+        driver = getChromeDriver();
+        getfd().get(properties.getProperty("sfdcurl"));
+        getfd().element(id("username")).waitUntil(30).ifElementIsNotDisplayed().clear().sendKeys(properties.getProperty("sfdcusername"));
+        getfd().element(id("password")).clear().sendKeys(properties.getProperty("sfdcpassword"));
+        getfd().element(id("Login")).click();
+        Thread.sleep(5000); // waiting for things to get settled
+        getfd().get(properties.getProperty("pushupgradeurl"));
+        getfd().select(xpath(SELECT_SFDC_VERSION)).waitUntil(30).ifElementIsNotDisplayed().selectByText(properties.getProperty("sfdcversion"));
+        getfd().select(xpath(ORGS_TYPE)).waitUntil(30).ifElementIsNotDisplayed().selectByText(properties.getProperty("orgType"));
+        getfd().select(xpath(CURRENT_SFDC_VERSION)).waitUntil(30).ifElementIsNotDisplayed().selectByText(properties.getProperty("currentsfdcversion"));
+        Map<String, Object> status = Maps.newHashMap();
+        FileWriter passedFile = new FileWriter(properties.getProperty("passedlist"));
+        FileWriter failedFile = new FileWriter(properties.getProperty("failedlist"));
+        try {
+            for (String org : orgs) {
+                getfd().element(xpath(ORG_SEARCH_BOX)).waitUntil(30).ifElementIsNotDisplayed().clear().sendKeys(org);
+                getfd().element(xpath(LOADING_ICON)).waitUntil(10).ignoringError().ifElementIsNotDisplayed().waitUntil(45).ifElementIsDisplayed();
+                Thread.sleep(1000); // waiting for things to get settled
+                if (getfd().element(xpath(SEARCH_RESULTS)).waitUntil(2).ignoringError().ifElementIsNotDisplayed().getElements().size() == 1) {
+                    String resultOrg = getfd().element(xpath(ORG_ID)).getText().trim();
+                    if (resultOrg.equalsIgnoreCase(org) || org.contains(resultOrg)) {
+                        getfd().checkbox(xpath(SELECT_ORG_ID)).check(() -> {
+                            boolean selected;
+                            selected = !getfd().checkbox(xpath(SELECT_ORG_ID)).isSelected();
+                            return selected;
+                        });
+                        status.put(org, "PASSED");
+                        System.out.println("Successfully selected OrgId " + org);
+                        passedFile.write(org + "\n");
+                    }
+                } else {
+                    status.put(org, "FAILED");
+                    System.out.println("Failed to select OrgId, Since search returned multiple results or org not found " + org);
+                    failedFile.write(org + "\n");
+                }
+            }
+            System.out.println("Final result is " + status.toString());
+        } finally {
+            failedFile.close();
+            passedFile.close();
+        }
+    }
+
+
+    private static void loadPropertiesfile() throws IOException {
+        properties = new Properties();
+        String propFileName = "app.properties";
+        InputStream inputStream = MethodHandles.lookup().lookupClass().getClassLoader().getResourceAsStream(propFileName);
+        if (inputStream != null) {
+            properties.load(inputStream);
+        } else {
+            throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+        }
+    }
+
+    private static List<String> getOrgDetails() throws IOException {
+        ClassLoader classLoader = MethodHandles.lookup().lookupClass().getClassLoader();
+        File file = new File(classLoader.getResource("orgs.csv").getFile());
+        List<String> lines = Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
+        List<String> orgs = Lists.newArrayList();
+        for (String line : lines) {
+            String[] array = line.split(",");
+            orgs.add(array[0].trim());
+        }
+        return orgs;
+    }
+
+    private static WebDriver getChromeDriver() {
+        String chromeDriverLocation = System.getProperty("webdriver.chrome.driver");
+        if (StringUtils.isEmpty(chromeDriverLocation)) {
+            throw new IllegalArgumentException("webdriver.chrome.driver property should not be null");
+        }
+        File chromeDriverExecutable = new File(chromeDriverLocation);
+        if (!chromeDriverExecutable.exists()) {
+            throw new AutomationException("webdriver.chrome.driver not found: " + chromeDriverLocation);
+        }
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        capabilities.setCapability("chrome.switches", Collections.singletonList("--start-maximized"));
+        driver = new ChromeDriver(capabilities);
+        driver.manage().window().maximize();
+        return driver;
+    }
+
+    private static FluentDriver getfd() {
+        if (fluentDriver == null)
+            initFluentDriver();
+        return fluentDriver;
+    }
+
+    private static void initFluentDriver() {
+        fluentDriver = new FluentDriver(driver);
+    }
+}
