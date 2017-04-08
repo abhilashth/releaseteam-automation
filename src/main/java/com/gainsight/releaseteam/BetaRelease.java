185ce9dfe189630dbe5c9614fdc18e549de91a9f
Abhilash Thaduka <athaduka@gainsight.com>
2017-04-08 22:55:08 +0530

    Added screenshot support

--- src/main/java/com/gainsight/releaseteam/BetaRelease.java
+++ src/main/java/com/gainsight/releaseteam/BetaRelease.java
@@ -1,17 +1,12 @@
 package com.gainsight.releaseteam;
 
-import com.gainsight.exceptions.AutomationException;
 import com.gainsight.pageobject.core.fluent.FluentDriver;
-import com.gainsight.releaseteam.utils.Screenshot;
+import com.gainsight.releaseteam.utils.CoreUtils;
 import com.gainsight.sfdc.SalesforceConnector;
 import com.gainsight.sfdc.beans.SFDCInfo;
 import com.gainsight.testdriver.Log;
-import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 
 import java.io.File;
@@ -20,7 +15,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.invoke.MethodHandles;
 import java.util.Calendar;
-import java.util.Collections;
 import java.util.Locale;
 import java.util.Properties;
 
@@ -32,24 +26,24 @@ import static org.openqa.selenium.By.xpath;
  */
 public class BetaRelease {
 
-    private static FluentDriver fluentDriver;
     private static WebDriver driver;
-    private static Properties properties;
     private static final String UPLOAD_BUTTON = "//input[@value='Upload']";
     private static final String FINAL_BETA_UPLOAD_BUTTON = "//input[contains(@id,'PackageDetailsPageBlockButtons:upload')]";
     private static final String UPLOAD_VERSION_NUMBER = "//span[contains(@id,'UploadVersionNumber')]";
     private static final String VERSION_NAME_INPUT = "//input[contains(@id,'VersionInfoSectionItem:VersionText')]";
     private static String confDir = ".";
     private static String basedir = ".";
+    private static Properties properties;
     private static String sfdcendpoint;
+    private static FluentDriver fluentDriver;
 
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws Exception {
 
         loadPropertiesfile();
         getSalesforceConnection();
-        //  driver = getChromeDriver();
-        driver = getFirefoxDriver();
+        //driver = CoreUtils.getChromeDriver();
+        driver = CoreUtils.getFirefoxDriver();
         getfd().get(properties.getProperty("sfdcurl"));
         getfd().element(id("username")).waitUntil(30).ifElementIsNotDisplayed().clear().sendKeys(properties.getProperty("sfdcusername"));
         getfd().element(id("password")).clear().sendKeys(properties.getProperty("sfdcpassword"));
@@ -57,7 +51,7 @@ public class BetaRelease {
         Thread.sleep(5000); // waiting for things to get settled
         Log.info("Opening url for beta upload " + sfdcendpoint);
         getfd().get(sfdcendpoint);
-        Screenshot.takeScreenShot(getfd());
+        CoreUtils.getscreenshot("Image-After-Opening-Url", driver);
         getfd().input(xpath(UPLOAD_BUTTON)).waitUntil(45).ifElementIsNotDisplayed().click();
         String versionNumber = getfd().element(xpath(UPLOAD_VERSION_NUMBER)).waitUntil(600).ifElementIsNotDisplayed().getText().trim();
         Calendar calendar = Calendar.getInstance();
@@ -68,50 +62,10 @@ public class BetaRelease {
         getfd().element(xpath(VERSION_NAME_INPUT)).waitUntil(30).ifElementIsNotDisplayed().clear().sendKeys(finalVersion);
         //final upload page
         getfd().input(xpath(FINAL_BETA_UPLOAD_BUTTON)).waitUntil(45).ifElementIsNotDisplayed().click();
+        CoreUtils.getscreenshot("Image-After-Beta-Upload", driver);
 
     }
 
-
-    private static void loadPropertiesfile() throws IOException {
-        properties = new Properties();
-        String propFileName = "betarelease.properties";
-        InputStream inputStream = MethodHandles.lookup().lookupClass().getClassLoader().getResourceAsStream(propFileName);
-        if (inputStream != null) {
-            properties.load(inputStream);
-        } else {
-            throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
-        }
-
-        File configFolder = new File(basedir + File.separator + "conf");
-        if (!configFolder.exists()) {
-            confDir = new File(new File("").getAbsolutePath()).toPath().getParent().toString();
-        } else
-            confDir = basedir;
-        PropertyConfigurator.configure(confDir + "/conf/log4j.properties");
-    }
-
-    private static WebDriver getFirefoxDriver() {
-        Log.info("Driver is set to Firefox.");
-        driver = new FirefoxDriver();
-        return driver;
-    }
-
-    private static WebDriver getChromeDriver() {
-        Log.info("Driver is set to Chrome.");
-        String chromeDriverLocation = System.getProperty("webdriver.chrome.driver");
-        if (StringUtils.isEmpty(chromeDriverLocation)) {
-            throw new IllegalArgumentException("webdriver.chrome.driver property should not be null");
-        }
-        File chromeDriverExecutable = new File(chromeDriverLocation);
-        if (!chromeDriverExecutable.exists()) {
-            throw new AutomationException("webdriver.chrome.driver not found: " + chromeDriverLocation);
-        }
-        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-        capabilities.setCapability("chrome.switches", Collections.singletonList("--start-maximized"));
-        driver = new ChromeDriver(capabilities);
-        driver.manage().window().maximize();
-        return driver;
-    }
 
     private static FluentDriver getfd() {
         if (fluentDriver == null)
@@ -131,6 +85,24 @@ public class BetaRelease {
         Assert.assertTrue(salesforceConnector.connect(), "Error while connecting to salesforce");
         SFDCInfo sfdcInfo = salesforceConnector.fetchSFDCinfo();
         sfdcendpoint = "https://" + sfdcInfo.getEndpoint().substring(8, sfdcInfo.getEndpoint().indexOf(".")) + ".salesforce.com/033U0000000CdVi?tab=PackageComponents";
+    }
+
+    public static void loadPropertiesfile() throws IOException {
+        properties = new Properties();
+        String propFileName = "betarelease.properties";
+        InputStream inputStream = MethodHandles.lookup().lookupClass().getClassLoader().getResourceAsStream(propFileName);
+        if (inputStream != null) {
+            properties.load(inputStream);
+        } else {
+            throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+        }
+
+        File configFolder = new File(basedir + File.separator + "conf");
+        if (!configFolder.exists()) {
+            confDir = new File(new File("").getAbsolutePath()).toPath().getParent().toString();
+        } else
+            confDir = basedir;
+        PropertyConfigurator.configure(confDir + "/conf/log4j.properties");
     }
 }
 
---
+++ src/main/java/com/gainsight/releaseteam/utils/CoreUtils.java
@@ -0,0 +1,58 @@
+package com.gainsight.releaseteam.utils;
+
+import com.gainsight.exceptions.AutomationException;
+import com.gainsight.testdriver.Log;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.util.Collections;
+
+/**
+ * Created by Abhilash Thaduka on 4/8/2017.
+ */
+public class CoreUtils {
+
+    private static WebDriver driver;
+
+    public static WebDriver getFirefoxDriver() {
+        Log.info("Driver is set to Firefox.");
+        driver = new FirefoxDriver();
+        return driver;
+    }
+
+    public static WebDriver getChromeDriver() {
+        Log.info("Driver is set to Chrome.");
+        String chromeDriverLocation = System.getProperty("webdriver.chrome.driver");
+        if (StringUtils.isEmpty(chromeDriverLocation)) {
+            throw new IllegalArgumentException("webdriver.chrome.driver property should not be null");
+        }
+        File chromeDriverExecutable = new File(chromeDriverLocation);
+        if (!chromeDriverExecutable.exists()) {
+            throw new AutomationException("webdriver.chrome.driver not found: " + chromeDriverLocation);
+        }
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        capabilities.setCapability("chrome.switches", Collections.singletonList("--start-maximized"));
+        driver = new ChromeDriver(capabilities);
+        driver.manage().window().maximize();
+        return driver;
+    }
+
+
+    public static void getscreenshot(String screenshotName, WebDriver driver) throws Exception {
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        //The below method will save the screen shot in d drive with name "screenshot.png"
+        FileUtils.copyFile(scrFile, new File("target/" + screenshotName + ".png"));
+        // FileUtils.copyFile(scrFile, new File(screenshotName + ".png"));
+    }
+
+
+}
+
+
